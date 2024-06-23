932dda6f10eb9c41388418b8c04694afc666721d
Joshua Daugherty <Joshua_Daugherty@insideranken.org>
2024-06-23 14:06:08 -0500

    Chapter 7 Work

---
+++ Labs/CH07/Chapter7MVC/MyWebsite/Areas/Help/Controllers/TutorialController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebsite.Areas.Help.Controllers
+{
+    public class TutorialController : Controller
+    {
+        [Area("Help")]
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
+		[Area("Help")]
+		public IActionResult Page2()
+		{
+			return View();
+		}
+
+		[Area("Help")]
+		public IActionResult Page3()
+		{
+			return View();
+		}
+	}
+}
